edf10e2673b75dca0d19a86419022c8489a865b4
Federico Zacayan <federico.zacayan@gmail.com>
2019-09-24 19:21:37 +1200

    Plaing nodemon as development dependency

--- app.js
+++ app.js
@@ -10,23 +10,23 @@ const orderRoutes = require('./api/routes/orders');
 mongoose.connect(
     "mongodb://mongo:27017/expressmongo" ,
     {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     }
 );
 mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).json({});
-  }
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
